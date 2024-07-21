31c92a5c4f6fb1a44eb174c727158c8e8e68b6be
aryadevapp <devapp.arya@gmail.com>
2024-07-21 09:42:18 +0800

    3. Struktur Folder

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,24 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('/about', function () {
+    return view('about',[
+        'name' => 'Galang Arya Tama'
+    ]);
+});
+
+
+// Untuk Mengirim data
+// Route::get('/about', function () {
+//     return view('about',[
+//         'name' => 'Galang Arya Tama'
+//     ]);
+// });
+
+// Hanya Menampilkan Teks
+// Route::get('/', function () {
+//     return 'Hello, World!';
+// });
